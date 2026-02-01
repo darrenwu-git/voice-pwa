33e425680705b5749732e22e66a88cb260177c9e
Darren Wu <darrenwu-git@users.noreply.github.com>
2026-02-01 14:07:19 +0800

    Add Service Worker for better PWA support

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'pippi-voice-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './src/style.css',
+  './src/app.js',
+  './manifest.json',
+  './public/icons/icon-192x192.png',
+  './public/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
